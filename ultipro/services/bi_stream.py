f8cc86f04c6f34217d1dd91cc9d6bcaa7a08376a
Jonathan Revah <jonathan.revah@equinox.com>
2019-05-08 15:16:19 -0400

    added a backoff decorator that indefinitely retries to fetch when the report is still being worked on. useful for heavy/long reports

--- ultipro/services/bi_stream.py
+++ ultipro/services/bi_stream.py
@@ -8,6 +8,7 @@ import backoff # Helps handle intermittent 405 errors from server
 endpoint = 'BiStreamingService'
 
 @backoff.on_exception(backoff.expo, requests.exceptions.HTTPError, max_tries=8, on_backoff=backoff_hdlr)
+@backoff.on_predicate(backoff.fibo, lambda x: x['header']['Status'] == 'Working', on_backoff=backoff_hdlr)
 def retrieve_report(client, report_key):
     zeep_client = ZeepClient(f"{client.base_url}{endpoint}")
     return zeep_client.service.RetrieveReport(_soapheaders={'ReportKey': report_key})
